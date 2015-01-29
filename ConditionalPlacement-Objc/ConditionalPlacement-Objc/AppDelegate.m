0010b14fa0b520b4c1548c4a37bf128a2488f2ed
Ross McIlwaine <rdm0423@gmail.com>
2015-01-29 14:36:53 -0700

    Completed ConditionalPlacement Assignment

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -19,6 +19,21 @@
 
     // Insert code here...
     
+    int karl = 10;
+    int leonard = 7;
+    int karlPlace = (karl > 7) ? 1 : 2;
+    int leonardPlace = (leonard > 7) ? 1 : 2;
+    
+    if (leonardPlace == 1 && karlPlace == 1) {
+        NSLog(@"Both Teachers are placed 1");
+    } else if (karlPlace ==1) {
+    NSLog(@"Karl is placed 1 and Leonard is placed 2");
+    } else if (leonardPlace ==1) {
+        NSLog(@"Leonard is placed 1 and Karl is placed 2");
+    } else if (leonardPlace != 1) {
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+    
     
     return YES;
 }
